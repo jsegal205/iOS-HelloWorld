af394174402e2ee29d37b1443d19cb1bbe6efaaa
jsegal205 <Jim@jimsegasmacbook.wowway.com>
2014-06-10 21:17:21 -0500

    Add HomeViewController

---
+++ iOS.HelloWorld/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  iOS.HelloWorld
+//
+//  Created by Jim Segal on 6/10/14.
+//  Copyright (c) 2014 JimLaptop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ iOS.HelloWorld/HomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.m
+//  iOS.HelloWorld
+//
+//  Created by Jim Segal on 6/10/14.
+//  Copyright (c) 2014 JimLaptop. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
